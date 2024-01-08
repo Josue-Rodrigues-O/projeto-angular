38e3dad6fd2c377ea38a42c798d6e2d454edb6a1
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2024-01-08 16:19:41 -0300

    -

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PaginaLoginLogonComponent } from './componentes/pagina-login-logon/pagina-login-logon.component';
+import { PaginaMeusCursosComponent } from './componentes/pagina-meus-cursos/pagina-meus-cursos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: PaginaLoginLogonComponent},
+    {path: 'MeusCursos', component: PaginaMeusCursosComponent}
+];
--- src/app/componentes/pagina-login-logon/pagina-login-logon.component.ts
+++ src/app/componentes/pagina-login-logon/pagina-login-logon.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-login-logon',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './pagina-login-logon.component.css'
 })
 export class PaginaLoginLogonComponent {
+  private Email: string = '';
+  private Senha: string = '';
+  private ConfirmarSenha: string = '';
 
+
+  constructor(
+    private router: Router
+  ){}
+
+  navegarParaMeusCursos(){
+    this.router.navigate(['/MeusCursos'])
+  }
+
+  cadastrarUsuario(){
+
+  }
 }
