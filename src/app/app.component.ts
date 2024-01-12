ce214a4ea354d7e0ec1239ccdc3672cdc758f2db
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2024-01-12 15:48:18 -0300

    tela login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BarraSuperiorComponent } from "./componentes/barra-superior/barra-superior.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet ]
+    imports: [CommonModule, RouterOutlet, BarraSuperiorComponent, RodapeComponent]
 })
 export class AppComponent {
 }
