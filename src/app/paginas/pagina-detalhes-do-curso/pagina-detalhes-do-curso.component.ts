902df7e7392e16d9eee38cd4f80189fd2a5ffd22
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2024-01-18 08:16:07 -0300

    -

--- src/app/paginas/pagina-detalhes-do-curso/pagina-detalhes-do-curso.component.ts
+++ src/app/paginas/pagina-detalhes-do-curso/pagina-detalhes-do-curso.component.ts
@@ -8,13 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './pagina-detalhes-do-curso.component.css'
 })
 export class PaginaDetalhesDoCursoComponent {
-  aoClicarNoTitulo(link: any) {
-    const video = document.getElementById("video")
-
-    video?.setAttribute("src", link)
-  }
-
-
   listaDeVideos = [
     { titulo: "Apresentação do curso", link: "https://www.youtube.com/embed/Ejkb_YpuHWs?si=QI2wbM1vpsGtHkot" },
     { titulo: "Aula 01", link: "https://www.youtube.com/embed/jgQjeqGRdgA?si=AbfflSu_TigL-aib" },
@@ -22,4 +15,28 @@ export class PaginaDetalhesDoCursoComponent {
     { titulo: "Aula 03", link: "https://www.youtube.com/embed/57wyfS560Uk?si=xOZ3CwonVPNu5Mq6" },
     { titulo: "Aula 04", link: "https://www.youtube.com/embed/0zLjVhHdOm8?si=zMIvtu3V0ysxQIas" }
   ]
+
+  concluido: boolean = false;
+  concluidoIcone: string = "radio_button_unchecked";
+  concluidoTexto: string = "Marcar como concluido"
+
+  aoClicarNoTitulo(link: any) {
+    const video = document.getElementById("video")
+    
+    video?.setAttribute("src", link)
+  }
+
+  aoClicarEmConcluido() {
+    if (this.concluido) {
+      this.concluidoIcone = "task_alt";
+      this.concluido = false;
+      this.concluidoTexto = "Concluido"
+    }else{
+      this.concluidoIcone = "radio_button_unchecked";
+      this.concluido = true;
+      this.concluidoTexto = "Marcar como concluido"
+    }
+  }
+
+
 }
